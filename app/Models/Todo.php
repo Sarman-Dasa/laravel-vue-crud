a32e90fde859be49133ee2da719381316fe9e2a1
Sarman-Dasa <sarmand@zignuts.com>
2023-05-03 19:06:50 +0530

    add column in fillable

---
+++ app/Models/Todo.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Todo extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description',
+    ];
+
+    protected $hidden = [
+        'created_at',
+        'updated_at',
+    ];
+}
