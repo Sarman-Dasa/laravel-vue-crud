8bbfa99122658bc53a078ba31c4208fd5279d5a8
Sarman-Dasa <sarmand@zignuts.com>
2023-05-11 19:30:44 +0530

    add mobile otp table

---
+++ database/migrations/2023_05_08_090211_create_user_otps_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_otps', function (Blueprint $table) {
+            $table->bigInteger('phone')->primary();
+            $table->integer('otp');
+            $table->timestamps();
+            $table->dateTime('expired_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_otps');
+    }
+};
