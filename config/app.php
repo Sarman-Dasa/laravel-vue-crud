60009de2554ff3ade94d136aa4d7137210e356d6
Sarman-Dasa <sarmand@zignuts.com>
2023-05-15 16:46:18 +0530

    add Excel aliase to access as facade class

--- config/app.php
+++ config/app.php
@@ -183,6 +183,7 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'Example' => App\Facades\Example::class,
+        'Excel' => Maatwebsite\Excel\Facades\Excel::class,
     ])->toArray(),
 
 ];
