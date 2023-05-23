10a0a1c94dd7ef71e62acb91ae25aa7cb36081c3
Sarman-Dasa <sarmand@zignuts.com>
2023-05-23 19:44:23 +0530

    add get method

---
+++ app/Http/Controllers/RoleController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Role;
+use Illuminate\Http\Request;
+
+class RoleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function list()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function create(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function get(string $id)
+    {
+        $role = Role::findOrFail($id);
+        return ok('Role get successfully',$role);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
