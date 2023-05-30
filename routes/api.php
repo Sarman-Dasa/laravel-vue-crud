f664daeb93fc3ccf5ef55c13b841c7e627ada8fd
Sarman-Dasa <sarmand@zignuts.com>
2023-05-30 19:18:40 +0530

    add employee module route

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,11 @@
 <?php
 
 use App\Http\Controllers\auth\AuthController;
+use App\Http\Controllers\EmployeeController;
 use App\Http\Controllers\RoleController;
 use App\Http\Controllers\TodoController;
 use App\Http\Controllers\UserController;
+use App\Models\Employee;
 use App\Models\Role;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -20,15 +22,15 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-Route::controller(TodoController::class)->prefix("todo")->group(function() {
-    Route::post('list','list');
-    Route::post('create','create');
-    Route::put('update/{id}','update');
-    Route::get('get/{id}','get');
-    Route::delete('delete/{id}','destroy');
-    Route::post('/file-upload','fileUpload');
-    Route::post('/export-data','export');
-    Route::put('update-status/{id}','status');
+Route::controller(TodoController::class)->prefix("todo")->group(function () {
+    Route::post('list', 'list');
+    Route::post('create', 'create');
+    Route::put('update/{id}', 'update');
+    Route::get('get/{id}', 'get');
+    Route::delete('delete/{id}', 'destroy');
+    Route::post('/file-upload', 'fileUpload');
+    Route::post('/export-data', 'export');
+    Route::put('update-status/{id}', 'status');
 });
 
 
@@ -41,30 +43,45 @@ Route::controller(AuthController::class)->group(function () {
     Route::post('login', 'login');
     Route::post('forgot-password', 'forgotPassword');
     Route::post('reset-password', 'resetPassword');
-    Route::post("login-with-mobile",'sendOtp')->name('sendOtp');
-    Route::post("verify-otp",'verifyOtp');
+    Route::post("login-with-mobile", 'sendOtp')->name('sendOtp');
+    Route::post("verify-otp", 'verifyOtp');
 });
 
-/**
- * User
- */
-Route::middleware(['auth:sanctum','throttle:1|30'])->group(function () {
+
+Route::middleware(['auth:sanctum'])->group(function () {
+    /**
+     * User
+     */
     Route::controller(UserController::class)->group(function () {
         Route::post('list', 'list')->middleware('role:superAdmin');
         Route::put('update', 'update');
         Route::get('get', 'get');
-        Route::delete('delete','destroy');
+        Route::delete('delete', 'destroy');
         Route::get('logout', 'logout');
         Route::post('change-password', 'changePassword');
-        Route::put('update-role/{id}','updateRole');
-        Route::put('update-status/{id}','updateStatus');
+        Route::put('update-role/{id}', 'updateRole');
+        Route::put('update-status/{id}', 'updateStatus');
+    });
+
+    /**
+     * Employee
+     */
+    Route::controller(EmployeeController::class)->prefix('employee')->group(function() {
+        Route::post('list', 'list');
+        Route::post('create', 'create');
+        Route::put('update/{id}', 'update');
+        Route::get('get/{id}', 'get');
+        Route::delete('delete/{id}', 'destroy');
     });
 });
 
-Route::controller(RoleController::class)->prefix('role')->group(function() {
-    Route::post('list','list');
-    Route::post('create','create');
-    Route::put('update/{id}','update');
-    Route::get('get/{id}','get');
-    Route::delete('delete/{id}','destroy');
+/**
+ * Role
+ */
+Route::controller(RoleController::class)->prefix('role')->group(function () {
+    Route::post('list', 'list');
+    Route::post('create', 'create');
+    Route::put('update/{id}', 'update');
+    Route::get('get/{id}', 'get');
+    Route::delete('delete/{id}', 'destroy');
 });
