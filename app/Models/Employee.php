4044b9890d04746c586c0963dd8f5399cf0a999e
Sarman-Dasa <sarmand@zignuts.com>
2023-05-31 18:52:45 +0530

    add hidden data to not use

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -10,4 +10,10 @@ class Employee extends Model
     use HasFactory;
 
     protected $fillable = ['name', 'email', 'phone', 'salary', 'joining_date'];
+
+    protected $hidden = [
+        'created_at',
+        'updated_at',
+        'deleted_at'
+    ];
 }
